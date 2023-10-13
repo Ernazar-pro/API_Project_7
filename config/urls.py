3353f025fa719d779f7588e1628e17961a6e55ba
Jumaniyazov Ernazar <jumaniyazovernazar88@gmail.com>
2023-10-13 16:17:37 +0500

    API_Project_7

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
+from config.settings import MEDIA_ROOT, MEDIA_URL
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
+
+urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT,}),]
